171ecaf227f4347f22a798a5e2dc0a6175a965ef
lopikorm <markavreliy@gmail.com>
2022-11-21 22:33:04 +0400

    Combined 'if', fix 'return mid_index'

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -11,18 +11,14 @@ module Exercise
         array.each_with_object([]) { |el, array_new| array_new << (el.positive? ? max(array) : el) }
       end
 
-      def search(array, query, index = 0)
-        first_index = 0
+      def search(array, query, first_index = 0, last_index = array.length - 1)
+        return -1 if array.size.zero? || (array[0] > query) || (array[-1] < query) || first_index > last_index
 
-        last_index = array.length - 1
+        mid_index = first_index + ((last_index - first_index) / 2)
 
-        array.size.zero? ? (return -1) : mid_index = first_index + ((last_index - first_index) / 2)
+        return mid_index if array[mid_index] == query
 
-        if (array[0] > query) || (array[-1] < query) then (return -1) end
-
-        if array[mid_index] == query then (return index + mid_index) end
-
-        array[mid_index] < query ? search(array[mid_index + 1, last_index], query, index + mid_index + 1) : search(array[first_index, mid_index], query, index)
+        array[mid_index] < query ? search(array, query, mid_index + 1, last_index) : search(array, query, first_index, mid_index - 1)
       end
     end
   end
